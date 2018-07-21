2607d0cbd33a52ec21e13c2628fd6637fa1fa098
V Saravanan <saravanan.resume@gmail.com>
2018-07-21 16:52:49 +0800

    minmax

--- velocity/Models/MinMax.cs
+++ velocity/Models/MinMax.cs
@@ -9,29 +9,37 @@ using velocity.Generic;
 namespace velocity.Models
 {
 
+    [Table("tbl_min_max")]
     public class MinMax : IPK<int>
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int id { get; set; }
-        public string tradeId { get; set; }
-        public string executionDt { get; set; }
+        public string transId { get; set; }
+        public string LEI { get; set; }
+        public string CIF { get; set; }
+        public string BuySell { get; set; }
+        public DateTime? executionDt { get; set; }
         public string ISIN { get; set; }
-        public string equity { get; set; }
-        public string quantity { get; set; }
-        public string price { get; set; }
-        public string feesSchedulePara { get; set; }
-        public DateTime effectiveDt { get; set; }
+        public string instrument { get; set; }
+        public decimal quantity { get; set; }
+        public decimal unitPrice { get; set; }
+        public decimal transactionCost { get; set; }
+        public decimal transactionFees { get; set; }
+        public decimal totalConsideration { get; set; }
+        public string feesScheduleParagraph { get; set; }
+        public DateTime? effectiveDt { get; set; }
         public decimal minFeePct { get; set; }
-        public decimal maxFeePct { get; set; }
         public decimal minFee { get; set; }
-        public string transactionCost { get; set; }
+        public decimal maxFeePct { get; set; }
+        public decimal maxFee { get; set; }
+        public decimal minFloor { get; set; }
         public string costIndicator { get; set; }
         public string country { get; set; }
-        public string businessFees { get; set; }
-        public string financialAssets { get; set; }
+        public string businessFeesInfo { get; set; }
+        public string financialAssetsType { get; set; }
         public string MIC { get; set; }
-        public string Currency { get; set; }
+        public string ccy { get; set; }
         public bool isActive { get; set; }
     }
 }
